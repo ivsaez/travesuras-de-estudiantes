a8856e0f94d7a9da28920d98ecb169b236b1b832
Ivan Saez Zamora <ivsaez@gmail.com>
2023-10-15 20:43:35 +0200

    remove legacy functions

--- src/logic/functions.ts
+++ src/logic/functions.ts
@@ -9,28 +9,7 @@ export class Functions{
         new Function("Estudiante", Cardinality.One),
         new Function("Profesor", Cardinality.One),
         new Function("Saludo", Cardinality.Two, "x", "y", true),
-        new Function("Pareja", Cardinality.Two, "x", "y", true),
-        
-        new Function("Comer", Cardinality.None),
-        new Function("Republicano", Cardinality.One),
-        new Function("Nacional", Cardinality.One),
-        new Function("Auxiliar", Cardinality.One),
-        new Function("Residente", Cardinality.One),
-        new Function("Impedido", Cardinality.One),
-        new Function("Demente", Cardinality.One),
-        new Function("Racista", Cardinality.One),
-        new Function("Medicado", Cardinality.One),
-        new Function("MedicadoFake", Cardinality.One),
-        new Function("Fumador", Cardinality.One),
-        new Function("Salido", Cardinality.One),
-        new Function("Pobre", Cardinality.One),
-        new Function("Descansado", Cardinality.One),
-        new Function("Cagado", Cardinality.One),
-        new Function("Agresor", Cardinality.One),
-        new Function("Recuerdo", Cardinality.One),
-        new Function("Bueno", Cardinality.One),
-        new Function("Malo", Cardinality.One),
-        
+        new Function("Pareja", Cardinality.Two, "x", "y", true)
     ];
 
     static get all(): Function[]{
